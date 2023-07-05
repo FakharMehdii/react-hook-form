091e2009c3317de0fc92c41703e7425d1e28f957
Fakhar Mehdi <fakhar.mehdi@mergestack.com>
2023-07-05 16:37:09 +0500

    ✨ inculcated further feedback

--- src/Components/InputField/index.js
+++ src/Components/InputField/index.js
@@ -7,7 +7,17 @@ const schema = yup.object().shape({
   taskTittle: yup
     .string()
     .required()
-    .max(35, "Task Tittle can contain 35 character only."),
+    .max(35, "Task Tittle can contain 35 character only.")
+    .min(3, "Task Tittle should be at least 3 characters long.")
+    .trim()
+    .test(
+      "non-numerical",
+      "NUMBERS NOT ALLOWED!!! Kindly Write numbers in alphabets...",
+      (value) => {
+        value=value.split("");
+        return !value.some((character) => character>='0' && character<='9')
+      }
+    ),
 });
 
 export const InputField = ({ addTodo }) => {
